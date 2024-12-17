7e2780bd7f810c3c1412176bc6e15c1b15f75f81
João Correia <joaocorreiaprof@gmail.com>
2024-12-17 20:21:46 +0000

    Fetch Genre component to display all genres available

--- client/src/components/Genres.jsx
+++ client/src/components/Genres.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import api from "../components/api";
+
 const Genres = () => {
-  return <h1>View all genres</h1>;
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/api/genres")
+      .then((response) => {
+        setGenres(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching genres", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Genres List:</h2>
+      {genres.length > 0 ? (
+        <ul>
+          {genres.map((genre, index) => (
+            <li key={index}>{genre.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No genres available</p>
+      )}
+    </div>
+  );
 };
 
 export default Genres;
