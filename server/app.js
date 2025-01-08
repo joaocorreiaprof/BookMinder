d465c853650b22fe55e34994843d6a7f51f6e731
João Correia <joaocorreiaprof@gmail.com>
2025-01-08 17:39:59 +0000

    update app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const pool = require("./db/db");
 
 // Route imports
@@ -18,6 +17,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Base route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Inventory Application API!");
+});
+
 // API routes
 app.use("/api", booksRoutes);
 app.use("/api", authorsRoutes);
@@ -26,26 +30,7 @@ app.use("/api", inventoryRoutes);
 app.use("/api", bookAuthorRoutes);
 app.use("/api", bookGenreRoutes);
 
-// For development, use Vite's server
-if (process.env.NODE_ENV === "development") {
-  // You do not need to do anything here for frontend routing; Vite handles it
-  app.use(express.static(path.join(__dirname, "client", "public")));
-} else {
-  // In production, serve static files from the build folder
-  app.use(express.static(path.join(__dirname, "client", "dist")));
-
-  // Catch-all to send React's index.html for any unmatched routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-  });
-}
-
-// Base route for API
-app.get("/", (req, res) => {
-  res.send("Welcome to the Inventory Application API!");
-});
-
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
