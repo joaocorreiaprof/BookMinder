809d81b53c357eae06a581adfa3f2c7a72bcb1d8
João Correia <joaocorreiaprof@gmail.com>
2025-01-08 17:06:41 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://inventory-application-production-4de2.up.railway.app/", // Redirects API calls to your backend
-    },
-  },
 });
