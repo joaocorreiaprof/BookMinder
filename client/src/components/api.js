05719a76568ef7fb9b0ab14f3a66a05fc5c1c3b7
João Correia <joaocorreiaprof@gmail.com>
2025-01-09 17:21:35 +0000

    Update api.js

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const isLocal = window.location.hostname === "localhost"; // Check if running locally
+
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: isLocal
+    ? "http://localhost:3000" // Local API endpoint
+    : "https://roundhouse.proxy.rlwy.net:50082/railway", // Railway API endpoint
 });
 
 export default api;
