dce09f9522e5f52730ec8b00e09dc1e69803fd98
João Correia <143901876+joaocorreiaprof@users.noreply.github.com>
2025-01-09 15:08:32 +0000

    Update api.js

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  connectionString:
+    "postgresql://postgres:ggXHEvsGhREBtzyBOtRiehmYQGKQUDXt@roundhouse.proxy.rlwy.net:50082/railway",
 });
 
 export default api;
