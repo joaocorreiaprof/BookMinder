15746674165852df1309232454db379d680ad2f0
João Correia <joaocorreiaprof@gmail.com>
2025-01-09 17:32:28 +0000

    Update api.js

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
   connectionString:
     "postgresql://postgres:ggXHEvsGhREBtzyBOtRiehmYQGKQUDXt@roundhouse.proxy.rlwy.net:50082/railway",
 });
